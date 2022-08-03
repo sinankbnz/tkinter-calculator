705bc3fa40beb90620394c4faf57af154bcbcdb3
sinankbnz <108318080+sinankbnz@users.noreply.github.com>
2022-08-03 09:45:43 +0530

    Delete 7.py

--- 7.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter first number :"))
-b=int(input("Enter second number :"))
-sum=a+b
-print("sum=",sum)
-if (sum%2)==0:
-    print("sum of",a,"and",b,"is even")
-else:
-    print("sum of",a,"and",b,"is odd")
